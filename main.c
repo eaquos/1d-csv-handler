b883d2416ee90db35e2675bc19c46eeeb74ef2da
Charlie Tahar <taharcharlie@gmail.com>
2018-09-09 16:51:08 +0700

    Update some file

--- main.c
+++ main.c
@@ -7,25 +7,23 @@ int main() {
     
     //** IMPORTANT PART **/
     // Variable Needed
-    int **output_array = calloc(1, sizeof(int*));
-    int *size = calloc(1,sizeof(int));
+    int *output_array;
+    int size;
     //********************/
 
     printf("Declare and Define the necessary variable\n");
 
-    readCSV("input.csv", output_array, size);
+    readCSV("input.csv", &output_array, &size);
 
     // Printout all the array content
-    for (int i = 0; i < *size; i++) {
-        printf("%d\n", (*output_array)[i]);
+    for (int i = 0; i < size; i++) {
+        printf("%d\n", output_array[i]);
     }
 
-    createCSV("output.csv", output_array, size);
+    createCSV("output.csv", &output_array, &size);
 
     //** IMPORTANT PART **/
-    free(*output_array);
     free(output_array);
-    free(size);
     //********************/
     return 0;
 }
